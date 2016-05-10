d1d0c780cb47b82ab8b9949bce3d31da20e717af
Dengqi <dengqi935@outlook.com>
2016-05-10 21:26:43 +0800

    bug fix
    bug fix

--- shell/shtech.sh
+++ shell/shtech.sh
@@ -3,7 +3,7 @@ echo "Shanghaitech Login Shell"
 echo "上科大登陆器shell版本"
 num=$#
 if [ "$num" = "2" ]; then
-       result=` curl -H "Content-Type: application/x-www-form-urlencoded" -X POST -s --data "userName=$1&password=$2&hasValidateCode=false&authLan=zh_CN"  --cookie "JSESSIONI$
+       result=` curl -H "Content-Type: application/x-www-form-urlencoded" -X POST -s --data "userName=$1&password=$2&hasValidateCode=false&authLan=zh_CN"  --cookie "JSESSIONID=D56359E00B58C7877668AAB44B3BFE31"  https://controller.shanghaitech.edu.cn:8445/PortalServer//Webauth/webAuthAction\!login.action`
         resultSucess=`echo "$result" | sed "s/.*success\W*\(\w*\)\W*total.*/\1/g"`
         #echo "$resultSucess"
         if [ "$resultSucess"x = "true"x ]; then
